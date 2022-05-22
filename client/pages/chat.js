9f2804f86a2d0759026d871faca1fd6fc61bf094
sami-lab <s.m.sami125@gmail.com>
2022-05-22 22:06:42 +0500

    test

---
+++ client/pages/chat.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Chat() {
+  return (
+    <iframe
+      width="350"
+      height="430"
+      allow="microphone;"
+      src="https://console.dialogflow.com/api-client/demo/embedded/d34275b0-41eb-4fe8-a31e-990447f65104"
+    ></iframe>
+  );
+}
